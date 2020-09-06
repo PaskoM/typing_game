89e52adfe10a6fbd76ee2c841886132017dbd651
Mariola Pasko <paskomariola@gmail.com>
2020-09-06 11:53:08 +0100

    js

--- script.js
+++ script.js
@@ -7,3 +7,59 @@ const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
 const difficultySelect = document.getElementById('difficulty');
+
+// List of words for game
+const words = [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'bad',
+    'north',
+    'dependent',
+    'steer',
+    'silver',
+    'highfalutin',
+    'superficial',
+    'quince',
+    'eight',
+    'feeble',
+    'admit',
+    'drag',
+    'loving'
+];
+
+// Init word
+let randomWord;
+
+// Init score
+let score = 0;
+
+// Init time
+let time = 10;
+
+// Set difficulty to value in ls or medium
+let difficulty =
+    localStorage.getItem('difficulty') !== null
+        ? localStorage.getItem('difficulty')
+        : 'medium';
+
+// Set difficulty select value
+difficultySelect.value =
+    localStorage.getItem('difficulty') !== null
+        ? localStorage.getItem('difficulty')
+        : 'medium';
+
+// Focus on text on start
+text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+// Generate random word from array
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
